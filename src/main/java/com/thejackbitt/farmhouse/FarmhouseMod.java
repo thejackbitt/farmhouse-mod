d21606216c9406edba31f65efb261b55f3d26e80
Davey-M <davey.meuer@gmail.com>
2024-03-29 23:27:29 -0500

    add farmhouse directory

---
+++ src/main/java/com/thejackbitt/farmhouse/FarmhouseMod.java
@@ -0,0 +1,8 @@
+package com.thejackbitt.farmhouse;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FarmhouseMod.MOD_ID)
+class FarmhouseMod {
+    public static final String MOD_ID = "farmhouse";
+}
