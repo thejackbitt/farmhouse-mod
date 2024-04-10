7899371d514f6a87224be59237f10e85cb6512b5
Davey-M <davey.meuer@gmail.com>
2024-04-09 21:44:49 -0500

    add frying pan as a pickaxe

--- src/main/java/com/thejackbitt/farmhouse/items/FryingPan.java
+++ src/main/java/com/thejackbitt/farmhouse/items/FryingPan.java
@@ -1,15 +1,56 @@
 package com.thejackbitt.farmhouse.items;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.Item.Properties;
 
 public class FryingPan {
     public static final String ID = "frying_pan";
 
     public static final Item build() {
-        return new Item(
+        return new PickaxeItem(
+            new FryingPanTier(),
+            0,
+            0f, // attack speed
             new Properties()
-        ); // TODO(Davey-M): add correct properties
+                .stacksTo(1)
+                .durability(100)
+        );
+    }
+
+    public static class FryingPanTier implements Tier {
+
+		@Override
+		public int getUses() {
+            return 0;
+		}
+
+		@Override
+		public float getSpeed() {
+            return 0f;
+		}
+
+		@Override
+		public float getAttackDamageBonus() {
+            return 0f;
+		}
+
+		@Override
+		public int getLevel() {
+            return 0;
+		}
+
+		@Override
+		public int getEnchantmentValue() {
+            return 0;
+		}
+
+		@Override
+		public Ingredient getRepairIngredient() {
+            return Ingredient.EMPTY;
+		}
     }
 }
 
